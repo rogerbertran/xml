03e9ac2db926f04ec7a4dd27e5229a30c96aad4f
Roger <roger.bertran.nebot@gmail.com>
2015-11-17 18:00:07 +0100

    a

---
+++ src/xml/Empleat.java
@@ -0,0 +1,66 @@
+package xml;
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement (name = "Treballador")
+@XmlType (propOrder = {"nom", "carrec", "edat", "sexe"})
+
+public class Empleat {
+	private int id;
+	private String sexe;
+	private int edat;
+	private String nom;
+	private String carrec;
+	private String password;
+	
+	public Empleat(int id, String sexe, int edat, String nom, String carrec, String password) {
+		super();
+		this.id = id;
+		this.sexe = sexe;
+		this.edat = edat;
+		this.nom = nom;
+		this.carrec = carrec;
+		this.password = password;
+	}
+	
+	@XmlAttribute
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSexe() {
+		return sexe;
+	}
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+	public int getEdat() {
+		return edat;
+	}
+	public void setEdat(int edat) {
+		this.edat = edat;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getCarrec() {
+		return carrec;
+	}
+	public void setCarrec(String carrec) {
+		this.carrec = carrec;
+	}
+	
+	@XmlTransient
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
