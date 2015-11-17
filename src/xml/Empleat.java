17b0d752ec12784ab5548933b8bb154282070b32
Roger <roger.bertran.nebot@gmail.com>
2015-11-17 18:30:43 +0100

    a

--- src/xml/Empleat.java
+++ src/xml/Empleat.java
@@ -1,6 +1,12 @@
 package xml;
-import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
 
+@XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement (name = "Treballador")
 @XmlType (propOrder = {"nom", "carrec", "edat", "sexe"})
 
---
+++ src/xml/Staff.java
@@ -0,0 +1,33 @@
+package xml;
+
+import java.util.ArrayList;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement (name = "Company_Staff")
+public class Staff {
+	
+	@XmlElement(name = "Pencaire", type = Empleat.class)
+	private ArrayList<Empleat> currantes = new ArrayList<Empleat>();
+
+	public ArrayList<Empleat> getCurrantes() {
+		return currantes;
+	}
+
+	public void setCurrantes(ArrayList<Empleat> currantes) {
+		this.currantes = currantes;
+	}
+
+	public Staff(ArrayList<Empleat> currantes) {
+		super();
+		this.currantes = currantes;
+	}
+	
+	public Staff() {
+		super();
+	}
+}
